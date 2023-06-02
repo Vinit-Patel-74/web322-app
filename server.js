2797ed2daac351ed2c1067ee3ee56245ff924533
Vinit Patel <patelvinitvp74@gmail.com>
2023-06-01 20:47:21 -0400

    changes to blog-service

--- server.js
+++ server.js
@@ -1,22 +1,60 @@
-var express = require("express");
-var app = express();
 
-var HTTP_PORT = process.env.PORT || 8080;
+const express = require('express');
+const app = express();
+const blogService = require('./blog-service');
 
-// Set up static file serving
-app.use(express.static('public'));
+const port = process.env.PORT || 8080;
+app.use(express.static('public/css'));
 
-// Setup a route to redirect "/" to "/about"
-app.get("/", (req, res) => {
-    res.redirect("/about");
+app.get('/', (req, res) => {
+  res.redirect('/about');
 });
 
-// Setup a route to return the about.html file from the views folder
-app.get("/about", (req, res) => {
-    res.sendFile(__dirname + "/views/about.html");
+app.get('/about', (req, res) => {
+  res.sendFile(__dirname+'/views/about.html');
 });
 
-// Start the server and listen on the specified port
-app.listen(HTTP_PORT, () => {
-    console.log("Express http server listening on port " + HTTP_PORT);
+app.get('/shop', (req, res) => {
+    blogService.getPublishedItems()
+      .then((publishedItems) => {
+        res.json(publishedItems);
+      })
+      .catch((error) => {
+        res.json({ message: error });
+      });
+  });
+  
+  app.get('/items', (req, res) => {
+    blogService.getAllItems()
+      .then((allItems) => {
+        res.json(allItems);
+      })
+      .catch((error) => {
+        res.json({ message: error });
+      });
+  });
+  
+  app.get('/categories', (req, res) => {
+    blogService.getCategories()
+      .then((allCategories) => {
+        res.json(allCategories);
+      })
+      .catch((error) => {
+        res.json({ message: error });
+      });
+  });
+  
+
+app.use((req, res) => {
+  res.status(404).send('Page Not Found');
 });
+
+blogService.initialize()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Express http server listening on port ${port}`);
+    });
+  })
+  .catch((error) => {
+    console.error('Initialization error:', error);
+  });
