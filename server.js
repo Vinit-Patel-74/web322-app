f1217bb053d14823a6c57e896fb9fe5d5e835815
Vinit Patel <patelvinitvp74@gmail.com>
2023-06-01 20:18:09 -0400

    initial commit

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
-var HTTP_PORT = process.env.PORT || 8080;
 var express = require("express");
 var app = express();
 
-// setup a 'route' to listen on the default url path
+var HTTP_PORT = process.env.PORT || 8080;
+
+// Set up static file serving
+app.use(express.static('public'));
+
+// Setup a route to redirect "/" to "/about"
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    res.redirect("/about");
 });
 
-// setup http server to listen on HTTP_PORT
-app.listen(HTTP_PORT);
+// Setup a route to return the about.html file from the views folder
+app.get("/about", (req, res) => {
+    res.sendFile(__dirname + "/views/about.html");
+});
+
+// Start the server and listen on the specified port
+app.listen(HTTP_PORT, () => {
+    console.log("Express http server listening on port " + HTTP_PORT);
+});
